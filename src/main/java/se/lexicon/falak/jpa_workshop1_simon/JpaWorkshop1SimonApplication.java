5c770dda2c35b8e2ee17137bfaa4373627baeb44
falakbarakat <45755774+falakbarakat@users.noreply.github.com>
2019-12-18 18:32:29 +0100

    jpa_simonm

---
+++ src/main/java/se/lexicon/falak/jpa_workshop1_simon/JpaWorkshop1SimonApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.falak.jpa_workshop1_simon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshop1SimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWorkshop1SimonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/falak/jpa_workshop1_simon/JpaWorkshop1SimonApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.falak.jpa_workshop1_simon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshop1SimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
